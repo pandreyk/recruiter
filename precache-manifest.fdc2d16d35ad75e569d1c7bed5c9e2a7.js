a53dcd28c9ab7943a710ba1a89d13e40445084c2
pandreyk <frosyavo@gmail.com>
2019-10-15 02:49:19 +0600

    Updates

---
+++ precache-manifest.fdc2d16d35ad75e569d1c7bed5c9e2a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6f70b3afa87000e4d3588467b11f5f",
+    "url": "/recruiter/index.html"
+  },
+  {
+    "revision": "004c69bff7ef3e4abe75",
+    "url": "/recruiter/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "28450691b6efa917a324",
+    "url": "/recruiter/static/css/main.193e1430.chunk.css"
+  },
+  {
+    "revision": "004c69bff7ef3e4abe75",
+    "url": "/recruiter/static/js/2.c878a715.chunk.js"
+  },
+  {
+    "revision": "28450691b6efa917a324",
+    "url": "/recruiter/static/js/main.814301f2.chunk.js"
+  },
+  {
+    "revision": "3cc9235e846a16988631",
+    "url": "/recruiter/static/js/runtime-main.25a21a24.js"
+  },
+  {
+    "revision": "a8e82b4255e5eb019bd3145b70a05186",
+    "url": "/recruiter/static/media/a.a8e82b42.png"
+  }
+]);
